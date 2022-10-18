98dc93a6807a1e5d6a3f7e74dd58120b7f8d1d7c
klaurier <laurier.kevin@gmail.com>
2022-10-18 16:38:37 +0200

    test

---
+++ builtin_pwd.c
@@ -0,0 +1,29 @@
+#include "minishell.h"
+//permet de faire la commande pwd sans argument ni option
+char *ft_builtin_pwd(int option)
+{
+	char	*path_dir;
+
+    path_dir = NULL;
+	
+	path_dir = getcwd(path_dir, 2000);
+    if (path_dir == NULL)
+    {    
+        free(path_dir);
+        return (NULL);
+    }
+    else if (path_dir != NULL && option == 1)
+    {
+		printf("%s\n", path_dir);
+    	free(path_dir);
+    	return (NULL);
+	}
+	else if (path_dir != NULL && option == 2)
+	{
+		return (path_dir);
+	}
+	free(path_dir);
+	return (NULL);
+	
+}
+
